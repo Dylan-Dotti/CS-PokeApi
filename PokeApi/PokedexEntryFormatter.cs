0beb01950c469239031d81737a757c4cddb20d06
Dylan Dotti <invocation42@yahoo.com>
2020-08-16 23:30:20 -0400

    Added

---
+++ PokeApi/PokedexEntryFormatter.cs
@@ -0,0 +1,14 @@
+﻿
+namespace PokeApi
+{
+    class PokedexEntryFormatter
+    {
+        public string FormatEntry(string entry)
+        {
+            entry = entry.Replace("\n", " ");
+            entry = entry.Replace("POKéMON", "Pokémon");
+            entry = entry.Replace("\f", " ");
+            return entry;
+        }
+    }
+}
---
+++ PokeApi/PokemonService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeApi
+{
+    class PokemonService : PokeApiService
+    {
+        public override string CategoryString => "pokemon";
+    }
+}
